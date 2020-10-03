b426fa2864ea33d72b1c01ab3fb925e781c4bb24
emanhamdy-a <0e0m0a0n0@gmail.com>
2020-10-03 17:39:51 +0200

    theme structure

---
+++ wp-content/themes/woocommercetheme/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+  <meta <?php bloginfo('charset');?>>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>
+    <?php  //wp_title('|','true','right');  bloginfo('name'); ?>
+  </title>
+  <link rel="pingback" href="<?php //bloginfo('pingback'); ?>"/>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class('test'); ?> >
+<div>
+  ggggggggg
+</div>
